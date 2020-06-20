ec596db7e584199f493eced491649f734d187e27
Anton Medwedew <medwedew.anton2017@yandex.ru>
2020-06-20 19:35:36 +0300

    Finish_add_form_QuizCreator, validate_this_form

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import classes from './QuizCreator.scss'
 import Button from '../../components/UI/Button/Button'
-import {createControl} from '../../form/formFramework'
+import {createControl, validate, validateForm} from '../../form/formFramework'
 import Input from '../../components/UI/Input/Input'
 import Select from '../../components/UI/Select/Select'
 
@@ -30,6 +30,7 @@ class QuizCreator extends Component {
 
   state = {
     rightAnswerId: 1,
+    isFormValid: false,
     quiz: [],
     formControls: createFormСontrols()
   }
@@ -38,16 +39,57 @@ class QuizCreator extends Component {
     event.preventDefault()
   }
 
-  addQuestionHandler = ()=> {
+  addQuestionHandler = event => {
+    event.preventDefault()
 
+    const quiz = this.state.quiz.concat()
+    const index = quiz.length + 1
+
+    //деструктуризация
+    const {question, option1, option2, option3, option4} = this.state.formControls
+
+    const questionItem = {
+      question: question.value,
+      id: index,
+      rightAnswerId: this.state.rightAnswerId,
+      answers: [
+        {text: option1.value, id: option1.id},
+        {text: option2.value, id: option2.id},
+        {text: option3.value, id: option3.id},
+        {text: option4.value, id: option4.id}
+      ]
+    }
+
+    quiz.push(questionItem)
+
+    this.setState({
+      quiz,
+      isFormValid: false,
+      rightAnswerId: 1,
+      formControls: createFormСontrols()
+    })
   }
 
-  createQuizHandler = ()=> {
+  createQuizHandler = event => {
+    event.preventDefault()
 
+    console.log(this.state.quiz)
   }
 
   changeHandler = (value, controlName) => {
+    const formControls = { ...this.state.formControls }
+    const control = { ...formControls[controlName] }
 
+    control.touched = true
+    control.value = value
+    control.valid = validate(control.value, control.validation)
+
+    formControls[controlName] = control
+
+    this.setState({
+      formControls, 
+      isFormValid: validateForm(formControls)
+    })
   }
   
   renderControls() {
@@ -59,10 +101,10 @@ class QuizCreator extends Component {
           <Input
             label={control.label}
             value={control.value}
-            errorMessage={control.errorMessage}
             valid={control.valid}
-            chouldValidatuon={!!control.validation}
+            shouldValidate={!!control.validation}
             touched={control.touched}
+            errorMessage={control.errorMessage}
             onChange={event => this.changeHandler(event.target.value, controlName)}
           />
           { index === 0 ? <hr /> : null }
@@ -105,6 +147,7 @@ class QuizCreator extends Component {
             <Button
               type="primary"
               onClick={this.addQuestionHandler}
+              disabled={!this.state.isFormValid}
             >
               Добавить вопрос
             </Button>
@@ -112,6 +155,7 @@ class QuizCreator extends Component {
             <Button
               type="success"
               onClick={this.createQuizHandler}
+              disabled={this.state.quiz.length === 0}
             >
               Создать тест
             </Button>
--- src/form/formFramework.js
+++ src/form/formFramework.js
@@ -1,3 +1,4 @@
+//validate form
 export function createControl(config, validation) {
   return {
     ...config,
@@ -6,4 +7,31 @@ export function createControl(config, validation) {
     touched: false,
     value: ''
   }
+}
+
+// validate form QuizCreator
+export function validate(value, validation = null) {
+  if (!validation) {
+    return true
+  }
+
+  let isValid = true
+
+  if (validation.required) {
+    isValid = value.trim() !== '' && isValid
+  }
+
+  return isValid
+}
+
+export function validateForm(formControls) {
+  let isFormValid = true
+
+  for (let control in formControls) {
+    if (formControls.hasOwnProperty(control)) {
+      isFormValid = formControls[control].valid && isFormValid
+    }
+  }
+
+  return isFormValid
 }
