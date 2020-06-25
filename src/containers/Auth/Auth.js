b03b56f911c6cd198e0b25326d9d9717f9795e7b
Anton Medwedew <medwedew.anton2017@yandex.ru>
2020-06-25 21:33:42 +0300

    realization_auth_firebase

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './Auth.scss'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
+import axios from 'axios'
 import is from 'is_js'
 
 class Auth extends Component {
@@ -36,11 +37,37 @@ class Auth extends Component {
     }
   }
 
-  loginHandler = ()=> {
+  loginHandler = async ()=> {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
 
+    try {
+      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyACvE4G7rjaAGTwZ1mypzjx0uG7DssUXIM', authData)
+      console.log(response.data)
+    }
+    catch (e) {
+      console.log(e)
+    } 
   }
 
-  registerHandler = ()=> {
+  registerHandler = async ()=> {
+    
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
+
+    try {
+      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyACvE4G7rjaAGTwZ1mypzjx0uG7DssUXIM', authData)
+      console.log(response.data)
+    }
+    catch (e) {
+      console.log(e)
+    } 
 
   }
 
