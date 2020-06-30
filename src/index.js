c82ab7fad7abca5c35bd563aed4ba8afccc8ef70
Anton Medwedew <medwedew.anton2017@yandex.ru>
2020-06-30 22:10:36 +0300

    add_package_redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import ReactDOM from 'react-dom'
+import './index.scss'
+import App from './App'
 import {BrowserRouter} from 'react-router-dom'
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker'
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import rootReducer from './store/reducers/rootReducer'
+import thunk from 'redux-thunk'
+
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
     <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     </React.StrictMode>,
   document.getElementById('root')
 );
